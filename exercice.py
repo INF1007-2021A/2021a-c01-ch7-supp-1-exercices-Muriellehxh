9fe48379ee6f2de084838a4311d8746c606bb442
Muriellehxh <dmui49@gmail.com>
2021-12-21 13:54:47 -0500

    ch7

--- exercice.py
+++ exercice.py
@@ -4,58 +4,75 @@
 from collections import deque
 
 
-def get_fibonacci_number(TODO):
-	pass
+def get_fibonacci_number(f):
+    if f==0:
+        return 0
+    elif f==1:
+        return 1
+    else:
+        return get_fibonacci_number(f-1) + get_fibonacci_number(f-2)
 
-def get_fibonacci_sequence(TODO):
-	pass
+def get_fibonacci_sequence(length, seq = [0, 1]):
+    if length <= 2:
+        return seq[0:length]
+    elif len(seq) < length:
+        return get_fibonacci_sequence(length, seq + [seq[-1] + seq[-2]])
+        # we get the last element with seq[-1], and the element right before it with seq[-2]
+    else:
+        return seq
 
-def get_sorted_dict_by_decimals(TODO):
-	pass
+
+def get_sorted_dict_by_decimals(dictionary):
+
+    return dict(sorted(dictionary.items(), key=lambda x: x[1] % 1.0))
+
+
 
 def build_recursive_sequence_generator(TODO):
-	pass
+
+
+    pass
 
 if __name__ == "__main__":
-	print([get_fibonacci_number(0), get_fibonacci_number(1), get_fibonacci_number(2)])
-	print([get_fibonacci_number(i) for i in range(10)])
-	print()
+    print([get_fibonacci_number(0), get_fibonacci_number(1), get_fibonacci_number(2)])
+    print([get_fibonacci_number(i) for i in range(10)])
+    print()
 
-	print(get_fibonacci_sequence(1))
-	print(get_fibonacci_sequence(2))
-	print(get_fibonacci_sequence(10))
-	print()
+    print(get_fibonacci_sequence(1))
+    print(get_fibonacci_sequence(2))
+    print(get_fibonacci_sequence(10))
+    print()
 
-	spam = {
-		2: 2.1,
-		3: 3.3,
-		1: 1.4,
-		4: 4.2
-	}
-	eggs = {
-		"foo": 42.6942,
-		"bar": 42.9000,
-		"qux": 69.4269,
-		"yeet": 420.1337
-	}
-	print(get_sorted_dict_by_decimals(spam))
-	print(get_sorted_dict_by_decimals(eggs))
-	print()
+    spam = {
+        2: 2.1,
+        3: 3.3,
+        1: 1.4,
+        4: 4.2
+    }
+    eggs = {
+        "foo": 42.6942,
+        "bar": 42.9000,
+        "qux": 69.4269,
+        "yeet": 420.1337
+    }
+    print(get_sorted_dict_by_decimals(spam))
+    print(get_sorted_dict_by_decimals(eggs))
+    print()
 
-	for fibo_num in fibonacci_numbers(10):
-		print(fibo_num, end=" ")
-	print("\n")
+    for fibo_num in get_fibonacci_number(10):
+        print(fibo_num, end=" ")
+    print("\n")
 
-	def fibo_def(last_elems):
-		return last_elems[-1] + last_elems[-2]
-	fibo = build_recursive_sequence_generator([0, 1], fibo_def)
-	for fi in fibo(10):
-		print(fi, end=" ")
-	print("\n")
+    def fibo_def(last_elems):
+        return last_elems[-1] + last_elems[-2]
+    fibo = build_recursive_sequence_generator([0, 1], fibo_def)
+    for fi in fibo(10):
+        print(fi, end=" ")
+    print("\n")
 
-	lucas = build_recursive_sequence_generator(TODO)
-	print(f"Lucas : {[elem for elem in lucas(10)]}")
-	perrin = build_recursive_sequence_generator(TODO)
-	print(f"Perrin : {[elem for elem in perrin(10)]}")
-	hofstadter_q = build_recursive_sequence_generator(TODO)
-	print(f"Hofstadter-Q : {[elem for elem in hofstadter_q(10)]}")
+    lucas = build_recursive_sequence_generator(TODO)
+    print(f"Lucas : {[elem for elem in lucas(10)]}")
+    perrin = build_recursive_sequence_generator(TODO)
+    print(f"Perrin : {[elem for elem in perrin(10)]}")
+    hofstadter_q = build_recursive_sequence_generator(TODO)
+    print(f"Hofstadter-Q : {[elem for elem in hofstadter_q(10)]}")
